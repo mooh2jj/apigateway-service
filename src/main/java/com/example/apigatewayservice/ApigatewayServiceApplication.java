9613e9a9e20cc273e61acbca6bad6455b6f88f02
mooh2jj@naver.com <62453668+mooh2jj@users.noreply.github.com>
2021-06-14 22:34:39 +0900

    api 게이트웨이 필터 filer Logging까지 구현

--- src/main/java/com/example/apigatewayservice/ApigatewayServiceApplication.java
+++ src/main/java/com/example/apigatewayservice/ApigatewayServiceApplication.java
@@ -3,11 +3,15 @@ package com.example.apigatewayservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+import java.util.List;
+
 @SpringBootApplication
 public class ApigatewayServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApigatewayServiceApplication.class, args);
+
     }
 
 }
